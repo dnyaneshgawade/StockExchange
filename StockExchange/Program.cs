54b16ab83e5b2b5e3326fad639863ebe235b0a98
dnyana <dnyaneshgawade903@gmail.com>
2021-08-02 10:37:30 +0530

    [Dnyanesh] add :Wellcome message

---
+++ StockExchange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Stock Exchange!");
+        }
+    }
+}
